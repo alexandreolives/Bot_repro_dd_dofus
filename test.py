667f5fca04fd61dd364bf2a8814acf9e95984e18
Plogeur <79613259+Plogeur@users.noreply.github.com>
2024-08-08 17:00:55 +0200

    Update test.py

--- test.py
+++ test.py
@@ -9,7 +9,6 @@ class TestGenealogie(unittest.TestCase):
         self.ggp2 = dd_class.Node("ggp2", 0)
         self.ggp3 = None
         self.ggp4 = None
-
         self.ggp5 = None
         self.ggp6 = None
         self.ggp7 = None
@@ -49,11 +48,24 @@ class TestCrosing(unittest.TestCase):
         self.dd_sm1 = dd_class.Dragodinde(100, "M", "Rousse", 1)
         self.dd_sm2 = dd_class.Dragodinde(101, "F", "Amande", 1)
 
-        #print("self.dd_s1 : ", self.dd_s1)
-
         # simple bi 
         self.dd_sb1 = dd_class.Dragodinde(200, "M", "Indigo et Orchidée", 6)
         self.dd_sb2 = dd_class.Dragodinde(201, "F", "Pourpre et Ivoire", 8)
+
+        # first bi
+        self.p1 = dd_class.Node("Rousse")
+        self.p2 = dd_class.Node("Dorée")
+        self.ind_1 = dd_class.Node("Rousse et Dorée", 0.5, self.p1, self.p2)
+        self.genealogie_1 = dd_class.Genealogie(self.ind_1)
+        self.genealogie_1.update_weights_and_colors()
+        self.dd_fb1 = dd_class.Dragodinde(300, "M", "Rousse et Dorée", 2, self.genealogie_1)
+
+        self.p1 = dd_class.Node("Amande")
+        self.p2 = dd_class.Node("Dorée")
+        self.ind_1 = dd_class.Node("Amande et Dorée", 0.5, self.p1, self.p2)
+        self.genealogie_1 = dd_class.Genealogie(self.ind_1)
+        self.genealogie_1.update_weights_and_colors()
+        self.dd_fb2 = dd_class.Dragodinde(301, "F", "Amande et Dorée", 2, self.genealogie_1)
 
         # Mono Amande
         self.gp1 = dd_class.Node("Ebène")
@@ -62,7 +74,7 @@ class TestCrosing(unittest.TestCase):
         self.genealogie_1 = dd_class.Genealogie(self.p1)
         self.genealogie_1.update_weights_and_colors()
         self.dd_1 = dd_class.Dragodinde(1, "M", "Amande", 1, self.genealogie_1)
-        
+
         # Mono Rousse
         self.gp3 = dd_class.Node("Indigo")
         self.gp4 = dd_class.Node("Rousse")
@@ -76,20 +88,15 @@ class TestCrosing(unittest.TestCase):
         self.ggp2 = dd_class.Node("Orchidée et Rousse")
         self.ggp3 = dd_class.Node("Rousse et Ebène")
         self.ggp4 = dd_class.Node("Turquoise et Rousse")
-       
         self.gp5 = dd_class.Node("Rousse et Prune", None, self.ggp1, self.ggp2)
         self.gp6 = dd_class.Node("Ebène et Ivoire", None, self.ggp3, self.ggp4)
-        self.p3 = dd_class.Node("Rousse et Pourpre", None, self.gp5, self.gp6)
-
+        self.p3 = dd_class.Node("Pourpre et Rousse", None, self.gp5, self.gp6)
         self.gp7 = dd_class.Node("Indigo et Orchidée")
         self.gp8 = dd_class.Node("Amande et Turquoise")
         self.p4 = dd_class.Node("Amande et Indigo", None, self.gp7, self.gp8)
-       
         self.ind_1 = dd_class.Node("Rousse et Amande", None, self.p3, self.p4)
         self.genealogie_3 = dd_class.Genealogie(self.ind_1)
         self.genealogie_3.update_weights_and_colors()
-
-        self.dd_3 = dd_class.Dragodinde(3, "M", "Rousse et Amande", 2, self.genealogie_3)
         self.dd_3 = dd_class.Dragodinde(3, "M", "Rousse et Amande", 2, self.genealogie_3)
 
         # Bi Pourpre et Orchidée  
@@ -97,28 +104,25 @@ class TestCrosing(unittest.TestCase):
         self.ggp6 = dd_class.Node("Turquoise et Rousse")
         self.ggp7 = dd_class.Node("Rousse et Prune")
         self.ggp8 = dd_class.Node("Dorée et Indigo")
-
         self.gp9 = dd_class.Node("Dorée et Emeraude", None, self.ggp5, self.ggp6)
         self.gp10 = dd_class.Node("Orchidée et Emeraude", None, self.ggp7, self.ggp8)
         self.p5 = dd_class.Node("Indigo et Ebène", None, self.gp9, self.gp10)
-
         self.gp11 = dd_class.Node("Amande et Ivoire")
         self.gp12 = dd_class.Node("Indigo et Ivoire")        
         self.p6 = dd_class.Node("Ivoire et Prune", None, self.gp11, self.gp12)
-
         self.ind_2 = dd_class.Node("Pourpre et Orchidée", None, self.p5, self.p6)
         self.genealogie_4 = dd_class.Genealogie(self.ind_2)
         self.genealogie_4.update_weights_and_colors()
-
         self.dd_4 = dd_class.Dragodinde(4, "F", "Pourpre et Orchidée", 6, self.genealogie_4)
 
-        self.elevage = dd_class.Elevage([self.dd_1, self.dd_2, self.dd_3, self.dd_4, self.dd_s1, self.dd_s2])
+        self.elevage = dd_class.Elevage([self.dd_1, self.dd_2, self.dd_3, self.dd_4, 
+                                         self.dd_sm1, self.dd_sm2, self.dd_sb1, self.dd_sb2,
+                                         self.dd_fb1, self.dd_fb2])
 
     def uni_test_bad_crosing(self):
         with self.assertRaises(ValueError) as context:
             self.elevage.accouplement_naissance(self.elevage.get_dd_by_id(1), self.elevage.get_dd_by_id(3))
 
-        self.assertEqual(str(context.exception), "Cannot breed dragodindes of the same sex.")
         self.assertEqual(str(context.exception), "Cannot breed dragodindes of the same sex.")
 
     def test_crosing_mono_mono(self):
@@ -144,8 +148,6 @@ class TestCrosing(unittest.TestCase):
             }
 
         print("dic_probability mono-bi : ", dic_probability, '\n')
-
-        dic_probability = {k: v * 100 for k, v in dic_probability.items()}
         self.assertEqual(dic_probability, expected_probability)
     
     def test_crosing_bi_bi(self):
@@ -155,8 +157,6 @@ class TestCrosing(unittest.TestCase):
             }
         
         print("dic_probability bi-bi : ", dic_probability, '\n')
-
-        dic_probability = {k: v * 100 for k, v in dic_probability.items()}
         self.assertEqual(dic_probability, expected_probability)
 
     def test_crosing_simple_mono_bi(self):
@@ -190,6 +190,20 @@ class TestCrosing(unittest.TestCase):
         print("dic_probability simple bi-bi : ", dic_probability, '\n')
         self.assertEqual(dic_probability, expected_probability)
 
+    def test_crosing_first_bi_bi(self):
+        _, dic_probability = self.elevage.accouplement_naissance(self.elevage.get_dd_by_id(300), self.elevage.get_dd_by_id(301))
+        expected_probability = {
+                "Rousse et Dorée" : 19.8,
+                "Amande et Dorée" : 19.8,
+                "Dorée" : 17.8,
+                "Amande" : 17.6,
+                "Rousse" : 17.6,
+                "Ebène" : 6.9,
+                "Rousse et Amande" : 0.7,
+            }
+        
+        print("dic_probability first bi-bi : ", dic_probability, '\n')
+        self.assertEqual(dic_probability, expected_probability)
 
 if __name__ == "__main__":
     unittest.main()
